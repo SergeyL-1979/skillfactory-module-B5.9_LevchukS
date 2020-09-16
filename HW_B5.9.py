1d130c3e13e5834a3c63f271232f8897ba9264a1
SergeyL-1979 <65560756+SergeyL-1979@users.noreply.github.com>
2020-09-16 22:45:18 +0300

    Create HW_B5.9.py

---
+++ HW_B5.9.py
@@ -0,0 +1,26 @@
+import time
+
+class Repeater:
+    def __init__(self, num_runs = 5):
+        self.num_runs = num_runs
+    def __call__(self, func):
+        def wrap():
+            start = time.time()
+            func()
+            print("Начало функции-обертки")
+            for i in range(self.num_runs):
+                func()
+            print("Конец функции обертки")
+            end = time.time()
+            print('[*] Время выполнения: {} sec.'.format((end-start)))
+        return wrap
+
+rep = Repeater()
+
+# Используем декоратор
+@rep
+def f():
+    print("Основная функция")
+
+# Теперь вызовем функцию
+f()
